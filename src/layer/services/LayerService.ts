dd402dcf7fc86ccc781d24685468c2020955c338
joehoppe <4017029+joehoppe@users.noreply.github.com>
2025-08-14 15:15:26 -0400

    Implement updateWeights so that the Perceptrons can be trained

--- src/layer/services/LayerService.ts
+++ src/layer/services/LayerService.ts
@@ -1,3 +1,4 @@
+import { BinaryStepService } from '../../activation/services/BinaryStepService';
 import { LoggerService } from '../../logger/Services/LoggerService';
 import { PerceptronService } from '../../perceptron/services/PerceptronService';
 
@@ -22,6 +23,37 @@ export class LayerService {
       const prediction = perceptron.predict(inputFeatures);
       this.#logger.debug(`Prediction Result`, perceptron);
       return prediction;
+    });
+  }
+
+  /* Gradually adjusts weights so the network makes better predictions
+      If an input contributed to a wrong prediction,
+      its weight gets adjusted proportionally to reduce future errors
+   */
+  updateWeights(
+    inputs: number[],
+    rawErrors: number[], // How incorrect the prediction was for each node
+    learningRate: number, // Positive - increase weights. Negative, decrease
+  ): void {
+    if (rawErrors.length !== inputs.length) {
+      throw new Error('Number of errors must match number of inputs');
+    }
+
+    this.#nodes.forEach((node, nodeIndex) => {
+      const currentWeights = node.getWeights();
+
+      if (node.activationService instanceof BinaryStepService) {
+        this.#logger.warn(
+          'This formula might need to be adjusted from the raw error to the derivative of the loss function',
+        );
+      }
+
+      const newWeights = currentWeights.map(
+        // perceptron learning rule (delta rule):
+        (weight, weightIndex) =>
+          weight + learningRate * rawErrors[nodeIndex] * inputs[weightIndex],
+      );
+      node.setWeights(newWeights);
     });
   }
 }
